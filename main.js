ff0d6592be72abbc2a3cb583604dc08720dad35e
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-05 14:14:29 +0200

    Render HTML file

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
-/* Hello World Programm */
+/* Main.js – sven.codes */
 
+// Modules
 var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-http.createServer(function(request, res) {
-    // Send the HTTP header
-    // HTTP Status: 200: OK
-    // Content-Type: text-plain
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+// Constants
+var Port = 8081;
+var ContentTypeHtml = {'Content-Type': 'text/html'};
 
-    // Send the response body as "Hello World"
-    res.end('Hello sven.codes webpage.\n')
-}).listen(8081)
+// Server
+http.createServer(function(request, response) {
+    // Parse request
+    var pathname = url.parse(request.url).pathname;
 
-console.log('Server running at http://localhost:8081/');
+    console.log("Request for " + pathname + " received.");
+
+    fs.readFile(pathname.substr(1), function(err, data){
+        if(err) {
+            response.writeHead(404, ContentTypeHtml);
+            response.write("Not found.");
+        } else {
+            response.writeHead(200, ContentTypeHtml);
+            response.write(data.toString());
+        }
+        response.end();
+    });
+
+}).listen(Port)
+
+console.log('Server running at http://localhost:' + Port + '/');
