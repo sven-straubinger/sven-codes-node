d21fa6038ae45f3ca1f97ae0dc4069eb65c504bd
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-05 14:36:54 +0200

    Add comments

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 /* Main.js – sven.codes */
 
+// Init modules
 var express = require('express');
 var app = express();
 
+// Define static folder
+app.use(express.static('public'));
+
+// Route for frontpage
 app.get('/', function(req, res) {
     res.send('Welcome');
 })
 
+// Init server
 var server = app.listen(8081, function() {
-
     var host = server.address().address
     var port = server.address().port
 
     console.log("Listening on http://$s:%s", host, port)
-
 })
