2bbc3f20c12559ec2fbfaccff8c07500962e78a7
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-27 20:59:46 +0200

    Fix variable

--- public/javascripts/individual-detail/individual-detail.component.js
+++ public/javascripts/individual-detail/individual-detail.component.js
@@ -3,7 +3,7 @@
 angular
   .module('individualDetail')
   .component('individualDetail', {
-    template: 'TBD for {{ctrl.slug}}',
+    template: 'TBD for {{$ctrl.slug}}',
     controller: [ '$routeParams',
       function IndividualDetailController($routeParams){
         this.slug = $routeParams.slug;
