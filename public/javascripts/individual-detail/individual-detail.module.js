a993f72ad2173293acda4ae8a8f156da9cc30059
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-27 20:46:42 +0200

    Update comment

--- public/javascripts/individual-detail/individual-detail.module.js
+++ public/javascripts/individual-detail/individual-detail.module.js
@@ -2,5 +2,6 @@
 
 // Define the `individualDetail` module
 angular.module('individualDetail', [
-  'ngRoute' // This module depends on the ngRoute module for providing the $routeParams
-]);
+  'ngRoute' // This module depends on the ngRoute module for providing the $routeParams.
+]);         // We are explicit about the dependecies of a sub-module and we don't rely on dependencies
+            // inherited from a parent module because that parent module might not be there some day.
