d74e7d8e1d147a668f521ec4883b1af88d597806
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-27 16:15:15 +0200

    Fix tests

--- karma.conf.js
+++ karma.conf.js
@@ -6,7 +6,9 @@ module.exports = function(config) {
     files: [
       './node_modules/angular/angular.js',
       './node_modules/angular-mocks/angular-mocks.js',
+      './public/javascripts/**/*.module.js',
       './public/javascripts/*!(.module|.spec).js',
+      './public/javascripts/**/*!(.module|.spec).js',
       './public/javascripts/**/*.spec.js'
     ],
 
