6905af1e7648d6f9346275c6d8ef559b43c4aa6a
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-27 21:10:17 +0200

    Read in template data.

--- public/javascripts/individual-detail/individual-detail.component.js
+++ public/javascripts/individual-detail/individual-detail.component.js
@@ -4,9 +4,16 @@ angular
   .module('individualDetail')
   .component('individualDetail', {
     templateUrl: 'javascripts/individual-detail/individual-detail.template.html',
-    controller: [ '$routeParams',
-      function IndividualDetailController($routeParams){
+    controller: [ '$http','$routeParams',
+      function IndividualDetailController($http, $routeParams){
+        var self = this;
         this.slug = $routeParams.slug;
+
+        $http.get('javascripts/data/individuals.json').then(function(response) {
+          alert(self.slug + ": " + JSON.stringify(response));
+          self.individual =response[0];
+        });
+
       }
     ]
   });
