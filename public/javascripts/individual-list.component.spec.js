331acd518f7af13a09f415be8d6801c8981b1cb9
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-27 15:28:48 +0200

    Update tests

---
+++ public/javascripts/individual-list.component.spec.js
@@ -0,0 +1,16 @@
+describe('individualList', function() {
+
+  // Load the module that contains the `individualList` component before each test
+  beforeEach(module('svenCodesNode'));
+
+  // Test the controller
+  describe('IndividualListController', function() {
+
+    it('should create a `individual` model with 3 individuals', inject(function($componentController) {
+      var ctrl = $componentController('individualList');
+      expect(ctrl.individuals.length).toBe(3);
+    }));
+
+  });
+
+});
