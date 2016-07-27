02f426e1e610c6a01ef2d0707c1de2745cdd46e1
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-27 16:44:37 +0200

    Update test

--- public/javascripts/individual-list/individual-list.component.spec.js
+++ public/javascripts/individual-list/individual-list.component.spec.js
@@ -8,10 +8,19 @@ describe('individualList', function() {
   // Test the controller
   describe('IndividualListController', function() {
 
-    it('should create a `individual` model with 3 individuals', inject(function($componentController) {
-      var ctrl = $componentController('individualList');
-      expect(ctrl.individuals.length).toBe(3);
+    var ctrl;
+
+    beforeEach(inject(function($componentController) {
+      ctrl = $componentController('individualList');
     }));
+
+    it('should create a `individual` model with 3 individuals', function() {
+      expect(ctrl.individuals.length).toBe(3);
+    });
+
+    it('should set default value for `orderProp` model', function() {
+      expect(ctrl.orderProp).toBe('order');
+    });
 
   });
 
