7b460399637229e2f66ba96b7f504c320f9d7805
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-28 14:46:04 +0200

    Replace $http with profile service

--- public/javascripts/individual-detail/individual-detail.component.js
+++ public/javascripts/individual-detail/individual-detail.component.js
@@ -4,19 +4,18 @@ angular
   .module('individualDetail')
   .component('individualDetail', {
     templateUrl: 'javascripts/individual-detail/individual-detail.template.html',
-    controller: [ '$http','$routeParams',
-      function IndividualDetailController($http, $routeParams){
+    controller: [ 'Profile','$routeParams',
+      function IndividualDetailController(Profile, $routeParams){
         var self = this;
         this.slug = $routeParams.slug;
+
+        self.individuals = Profile.query(function() {
+          self.setImage('http://placehold.it/350x150');
+        });
 
         self.setImage = function(imageUrl) {
           self.mainImageUrl = imageUrl;
         }
-
-        $http.get('javascripts/data/individuals.json').then(function(response) {
-          self.individuals = response.data;
-          self.setImage('http://placehold.it/350x150');
-        });
 
       }
     ]
