08271c8849b6c43dd734ffdc6128775ea463aed3
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-28 14:30:46 +0200

    Setup dependency to core.profile

--- public/javascripts/individual-detail/individual-detail.module.js
+++ public/javascripts/individual-detail/individual-detail.module.js
@@ -2,6 +2,6 @@
 
 // Define the `individualDetail` module
 angular.module('individualDetail', [
-  'ngRoute' // This module depends on the ngRoute module for providing the $routeParams.
-]);         // We are explicit about the dependecies of a sub-module and we don't rely on dependencies
-            // inherited from a parent module because that parent module might not be there some day.
+  'ngRoute',      // This module depends on the ngRoute module for providing the $routeParams.
+  'core.profile'  // We are explicit about the dependecies of a sub-module and we don't rely on dependencies
+]);               // inherited from a parent module because that parent module might not be there some day.
