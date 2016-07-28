ad8c322624d10baa36c641579913918bc6492700
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-28 11:47:00 +0200

    Update test

--- public/javascripts/individual-detail/individual-detail.component.spec.js
+++ public/javascripts/individual-detail/individual-detail.component.spec.js
@@ -2,4 +2,22 @@
 
 describe('individualDetail', function() {
 
+  // Load module
+  beforeEach(module('individualDetail'));
+
+  // Test controller
+  describe('IndividualDetailController', function() {
+    var ctrl;
+
+    // Inject componentController provider
+    beforeEach(inject(function($componentController) {
+      ctrl = $componentController('individualDetail');
+    }));
+
+    if('should do something', function() {
+        expect(ctrl.slug).toBe('undfined');
+    });
+
+  });
+
 });
