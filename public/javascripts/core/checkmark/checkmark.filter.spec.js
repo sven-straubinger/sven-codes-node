f2f1a2922df47c6cbe675d6ddea924a802061f52
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-28 12:17:34 +0200

    Fix tests for filter

--- public/javascripts/core/checkmark/checkmark.filter.spec.js
+++ public/javascripts/core/checkmark/checkmark.filter.spec.js
@@ -6,8 +6,8 @@ describe('checkmark', function() {
 
   it('should convert boolean values to unicode checkmark or cross', function() {
     inject(function(checkmarkFilter) { // when injecting a filter, we need to suffix its name with 'Filter'
-      expect(checkmarkFilter(true)).toBe('sd');
-      expect(checkmarkFilter(false)).toBe('x');
+      expect(checkmarkFilter(true)).toBe('\u2713');
+      expect(checkmarkFilter(false)).toBe('\u2718');
     });
   });
 
