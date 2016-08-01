5368c2633caaed859bdcaad93a55c54de35fbc51
Sven Straubinger <straubinger.es@googlemail.com>
2016-08-01 18:55:13 +0200

    Update realm

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ var admins = {
 module.exports = function(req, res, next) {
   var user = auth(req);
   if (!user || !admins[user.name] || admins[user.name].password !== user.pass) {
-    res.set('WWW-Authenticate', 'Basic realm="example"');
+    res.set('WWW-Authenticate', 'Basic realm="sven.codes"');
     return res.status(401).send();
   }
   return next();
