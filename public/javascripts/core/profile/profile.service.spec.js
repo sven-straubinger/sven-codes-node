7cac69098e3fd3d93642d648ac25044c9d34565e
Sven Straubinger <straubinger.es@googlemail.com>
2016-08-04 19:13:17 +0200

    Setup test for GET endpoint

--- public/javascripts/core/profile/profile.service.spec.js
+++ public/javascripts/core/profile/profile.service.spec.js
@@ -4,6 +4,7 @@ describe('Profile', function() {
 
   var $httpBackend;
   var Profile;
+  var profileData  = { slug: 'sven' }
   var profilesData = [
     {slug: 'Profile X'},
     {slug: 'Profile Y'},
@@ -21,7 +22,9 @@ describe('Profile', function() {
   // Instantiate the service and "train" `$httpBackend` before each test
   beforeEach(inject(function(_Profile_, _$httpBackend_) {
     $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/profiles').respond(profilesData);
+
+    $httpBackend.when('GET', '/api/profiles').respond(profilesData);
+    $httpBackend.when('GET', '/api/profiles/sven').respond(profileData);
 
     Profile = _Profile_;
   }));
@@ -32,12 +35,20 @@ describe('Profile', function() {
     $httpBackend.verifyNoOutstandingRequest();
   });
 
-  it('should fetch profile data', function() {
+  it('should fetch all profiles', function() {
     var profiles = Profile.query();
     expect(profiles).toEqual([]);
 
     $httpBackend.flush();
     expect(profiles).toEqual(profilesData);
+  });
+
+  it('should fetch one profile', function() {
+    var profile = Profile.get({slug: 'sven'});
+    expect(profile).toEqual({});
+
+    $httpBackend.flush();
+    expect(profile).toEqual(profileData);
   });
 
 })
