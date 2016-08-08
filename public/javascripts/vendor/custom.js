7d13f9c4d99e67d1fd82c25c6fd154fc88b0607f
Sven Straubinger <straubinger.es@googlemail.com>
2016-08-08 15:41:18 +0200

    Add JavaScript to scroll to sections

--- public/javascripts/vendor/custom.js
+++ public/javascripts/vendor/custom.js
@@ -5,3 +5,14 @@ $("#scroll-indicator").click(function() {
   $("html, body").animate({ scrollTop: $(document).height()-$(window).height() }, 400, "swing");
   return false;
 });
+
+// Scroll to sections
+$("*[href='#!/#projects']").click(function() {
+  $("html, body").animate({ scrollTop: $('#projects').position().top }, 500, "swing");
+  return false;
+});
+
+$("*[href='#!/#accounts']").click(function() {
+  $("html, body").animate({ scrollTop: $('#accounts').position().top }, 600, "swing");
+  return false;
+});
