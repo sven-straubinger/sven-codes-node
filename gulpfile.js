15f1cb0ea7767d25969c08becc27d8ddcf3d23e8
Sven Straubinger <straubinger.es@googlemail.com>
2016-08-09 17:59:21 +0200

    Install gulp-nodemon and gulp-notify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
+// Dependencies
 var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+var notify = require('gulp-notify');
 
+// Gulp's default task
 gulp.task('default', function() {
-  // place code for your default task here
-});
+    nodemon({ // Configure nodemon to run & track project changes
+        script: './bin/www', // the script to run the app
+        ext: ''
+    }).on('restart', function(){
+        gulp.src('*')
+            .pipe(notify('Recompiling ...'));
+    })
+})
