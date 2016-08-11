d42d592c1bf14ba4d490b210ba80bf87f4ee508f
Sven Straubinger <straubinger.es@googlemail.com>
2016-08-11 15:13:47 +0200

    Update comment

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var notify = require('gulp-notify');
 
-// Gulp's default task
+// Debug task
 gulp.task('debug', function() {
     nodemon({ // Configure nodemon to run & track project changes
         script: './bin/www', // the script to run the app
