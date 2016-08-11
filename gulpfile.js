80b49e42aee5e0fdac2bf7be7414c5847a539467
Sven Straubinger <straubinger.es@googlemail.com>
2016-08-11 15:13:02 +0200

    Move debug script to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,11 @@ var nodemon = require('gulp-nodemon');
 var notify = require('gulp-notify');
 
 // Gulp's default task
-gulp.task('default', function() {
+gulp.task('debug', function() {
     nodemon({ // Configure nodemon to run & track project changes
         script: './bin/www', // the script to run the app
-        ext: ''
+        ext: '',
+        env: {'DEBUG': 'sven-codes-node:*'} // Expression matches: `DEBUG=sven-codes-node:* node ./bin/www`
     }).on('restart', function(){
         gulp.src('*')
             .pipe(notify('Recompiling ...'));
