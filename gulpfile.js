2ed0fb3d1fd89d1a2c319259cc8f99b07d1388f8
Sven Straubinger <straubinger.es@googlemail.com>
2016-08-11 16:16:52 +0200

    Install gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 // Dependencies
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var concat = require('gulp-concat');
 
 // Debug task
 gulp.task('debug', function() {
     nodemon({ // Configure nodemon to run & track project changes
         script: './bin/www', // the script to run the app
         ext: '',
-        env: {'DEBUG': 'sven-codes-node:*'} // Expression matches: `DEBUG=sven-codes-node:* node ./bin/www`
-    }).on('restart', function(){
-        gulp.src(['./public/javascripts/*.js']);
+        env: {'DEBUG': 'sven-codes-node:*'}, // Expression matches: `DEBUG=sven-codes-node:* node ./bin/www`
+        watch: [
+            'public/javascripts'
+        ],
+        tasks: ['concat']
     })
+})
+
+gulp.task('concat', function () {
+    gulp.src(['public/javascripts/*.js'])
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./public/dist/'));
 })
