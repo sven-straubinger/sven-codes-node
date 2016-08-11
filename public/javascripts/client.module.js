962ba35dd96712051d7985d0ba7b48415da01651
Sven Straubinger <straubinger.es@googlemail.com>
2016-08-11 17:03:00 +0200

    Remove `use strict` declaration

--- public/javascripts/client.module.js
+++ public/javascripts/client.module.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // Define the `svenCodesNode` module ...
 angular.module('svenCodesNode', [
   // ... which depends on following modules:
--- public/javascripts/core/checkmark/checkmark.filter.js
+++ public/javascripts/core/checkmark/checkmark.filter.js
@@ -1,5 +1,3 @@
-'use strict';
-
 angular
   .module('core')
   .filter('checkmark', function() {
